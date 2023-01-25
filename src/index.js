882829746e409ee2f221b2111b3f9d961b036da9
Nastya <asenka0301@gmail.com>
2023-01-25 17:18:17 +0300

    Install Webpack, Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log('Hello World!');
+import '../src/scss/style.scss';
+import 'bootstrap';
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,48 +1,42 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
-  entry: './src/index.js',
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: '/src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js'
   },
-  devServer: {
-    open: true,
-    host: 'localhost',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-
-    // Add your rules for custom modules here
-    // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-  } else {
-    config.mode = 'development';
-  }
-  return config;
 };
